2bede696dc0fefaa1d8e4afb6fdfb78999773237
Anish <anishtanvar@gmail.com>
2024-10-12 09:53:35 +0530

    All setup configurd

--- src/app.js
+++ src/app.js
@@ -13,9 +13,14 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-
 app.use(cookieParser())
 
+// Route Import
+import userRouter from "./routes/user.routes.js";
+
+
+// Define Route
+app.use("/api/v1/user", userRouter);
 
 
 export { app }
